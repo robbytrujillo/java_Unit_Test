e0b1e5aed7c69d3ef8a54425221f3ecd4eff23cd
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-18 11:38:10 +0700

    Up Belajar Java Unit Test Menggunakan Assertions

--- src/test/java/programmer/zaman/now/test/CalculatorTest.java
+++ src/test/java/programmer/zaman/now/test/CalculatorTest.java
@@ -1,12 +1,29 @@
 package programmer.zaman.now.test;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+
     private Calculator calculator = new Calculator();
+
     @Test
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
 //        var result = Calculator.add(10, 10);
+        assertEquals(20, result);
+//        if(result != 20) {
+//            throw new RuntimeException("Test gagal");
+//        }
+
+    }
+    @Test
+    public void testDivideSuccess() {
+        var result = calculator.devide(100, 10);
+        assertEquals(10, result);
+    }
+
+    public void testDivideFailed() {
+        calculator.devide(10, 0);
     }
 }
