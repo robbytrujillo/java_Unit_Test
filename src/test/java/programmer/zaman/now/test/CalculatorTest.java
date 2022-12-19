4241a3afe5d7f8505b18bd30aa837c9d4673da9d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-12-19 22:54:13 +0700

    Up Belajar Java Unit Test Mengubah Nama Unit Test

--- src/test/java/programmer/zaman/now/test/CalculatorTest.java
+++ src/test/java/programmer/zaman/now/test/CalculatorTest.java
@@ -1,13 +1,16 @@
 package programmer.zaman.now.test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test untuk kalkulator")
 public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("Test sekenario sukses untuk method add(integer, integer)")
     public void testAddSuccess() {
         var result = calculator.add(10, 10);
 //        var result = Calculator.add(10, 10);
