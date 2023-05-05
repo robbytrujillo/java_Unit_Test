99cf4df6ae9c09c5a995b773269cd3a9deda3d5d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-05 10:48:39 +0700

    urutan eksekusi test - OrderedAnnotation

--- src/test/java/programmer/zaman/now/test/OrderedTest.java
+++ src/test/java/programmer/zaman/now/test/OrderedTest.java
@@ -1,23 +1,27 @@
 package programmer.zaman.now.test;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
 
     @Test
+    @Order(1)
     void test3() {
 
     }
 
     @Test
+    @Order(3)
     void test2() {
 
     }
 
     @Test
+    @Order(2)
     void test1() {
 
     }
