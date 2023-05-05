33255dbe0b8502a438cddb5bdb5112a409d76036
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-05 10:45:15 +0700

    urutan eksekusi test - OrderedTest

---
+++ src/test/java/programmer/zaman/now/test/OrderedTest.java
@@ -0,0 +1,26 @@
+package programmer.zaman.now.test;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class OrderedTest {
+
+    @Test
+    void test3() {
+
+    }
+
+    @Test
+    void test2() {
+
+    }
+
+    @Test
+    void test1() {
+
+    }
+
+
+}
