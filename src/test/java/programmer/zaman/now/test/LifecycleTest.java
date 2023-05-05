e490100563f31300a0f4c8e14d5da0518d15875a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-05 10:34:32 +0700

    urutan eksekusi test - LifecycleTest

---
+++ src/test/java/programmer/zaman/now/test/LifecycleTest.java
@@ -0,0 +1,20 @@
+package programmer.zaman.now.test;
+
+import org.junit.jupiter.api.Test;
+
+public class LifecycleTest {
+
+    private String temp;
+
+    @Test
+    void testA() {
+        temp = "Robby";
+    }
+
+    @Test
+    void testB() {
+        System.out.println(temp);
+    }
+
+
+}
