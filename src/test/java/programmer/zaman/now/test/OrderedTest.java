6f44a34c969f69e29917a00b8a7d6a8a2909c161
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-05-07 23:01:50 +0700

    counter++;t

--- src/test/java/programmer/zaman/now/test/OrderedTest.java
+++ src/test/java/programmer/zaman/now/test/OrderedTest.java
@@ -8,22 +8,27 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class OrderedTest {
 
+    private int counter = 0;
+
     @Test
     @Order(1)
     void test3() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(3)
     void test2() {
-
+        counter++;
+        System.out.println(counter);
     }
 
     @Test
     @Order(2)
     void test1() {
-
+        counter++;
+        System.out.println(counter);
     }
 
 
