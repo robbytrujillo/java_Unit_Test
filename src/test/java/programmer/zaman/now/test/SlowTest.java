07aa7367fb529d38bd38b49e1e6e6b6965347a6d
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-06 06:32:06 +0700

    Java Unit Test | Ekesekusi Test Secara Paralel

--- src/test/java/programmer/zaman/now/test/SlowTest.java
+++ src/test/java/programmer/zaman/now/test/SlowTest.java
@@ -2,15 +2,28 @@ package programmer.zaman.now.test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
+@Execution(ExecutionMode.CONCURRENT)
 public class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
-    void testSlow() throws InterruptedException{
+    void testSlow1() throws InterruptedException{
         // jika unit testnya terlalu lama maka akan gagal eksekusinya
-        Thread.sleep(10_000);
+        Thread.sleep(4_000);
+    }@Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow2() throws InterruptedException{
+        // jika unit testnya terlalu lama maka akan gagal eksekusinya
+        Thread.sleep(4_000);
+    }@Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void testSlow3() throws InterruptedException{
+        // jika unit testnya terlalu lama maka akan gagal eksekusinya
+        Thread.sleep(4_000);
     }
 }
