190e1c086c50aeb3aa1b0ddc99c5a5c9e35fcde1
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-06-07 14:11:39 +0700

    Java Unit Test | Mockito | Pengenalan Macking

---
+++ src/main/java/programmer/zaman/now/test/data/Person.java
@@ -0,0 +1,54 @@
+package programmer.zaman.now.test.data;
+
+import java.util.Objects;
+
+public class Person {
+
+    private String id;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getName(), person.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/programmer/zaman/now/test/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package programmer.zaman.now.test.repository;
+
+import programmer.zaman.now.test.data.Person;
+
+public interface PersonRepository {
+
+    Person.selectById
+}
