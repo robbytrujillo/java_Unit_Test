101c60604bc88a5fd7bf188f18c16349ab84bba0
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-09-30 05:41:18 +0700

    Java Unit Test | Verifikasi di Mocking

--- src/main/java/programmer/zaman/now/test/repository/PersonRepository.java
+++ src/main/java/programmer/zaman/now/test/repository/PersonRepository.java
@@ -6,4 +6,6 @@ import programmer.zaman.now.test.data.Person;
 public interface PersonRepository { // jembatan antara program kita dan database
 
     Person selectById(String id); // parameter id
+
+    void insert(Person person);
 }
